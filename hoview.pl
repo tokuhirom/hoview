96d07b89d3682c9833439b812aeb6e01a31006d3
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-12-13 04:07:16 +0900

    - gen menu - etc.

--- hoview.pl
+++ hoview.pl
@@ -10,19 +10,37 @@ use DateTime;
 use Path::Class qw/dir/;
 
 my $render_entryingPerChild = 1000;
-my $inputdir = '/Users/tokuhirom/share/howm/articles/';
-my $outputdir = '/tmp/yoyo';
+my $inputdir = '/home/tokuhirom/dev/gp.ath.cx/data/';
+my $outputdir = '/usr/local/webapp/gp.ath.cx/public/memo/';
 my $entry_tmpl = <<'...';
 ? my ($title, $body, $lastmodified) = @_;
 <!doctype html>
 <html>
 <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title><?= $title ?> - tokuhirom's memo</title>
+    <meta http-equiv="Content-Style-Type" content="text/css" />  
+    <meta http-equiv="Content-Script-Type" content="text/javascript" />  
+    <link href="/static/css/reset.css" rel="stylesheet" type="text/css" />
+    <link href="/static/css/gpath.css" rel="stylesheet" type="text/css" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js" type="text/javascript"></script>
+    <script>
+        $(function () {
+            $("#menuContainer").load("/memo/menu.html");
+        });
+    </script>
 </head>
 <body>
-    <h1><?= $title ?> - tokuhirom's memo</h1>
-    <?= $body ?>
-    <div class="lastmodified">Last Modified: <?= $lastmodified ?></div>
+    <div id="container">
+        <div id="bodyContainer">
+            <h1 class="entry-title"><?= $title ?></h1>
+            <div class="entry-content"><?= $body ?></div>
+            <div class="updated">Last Modified: <?= $lastmodified ?></div>
+        </div>
+        <div id="menuContainer">
+            now loading
+        </div>
+    </div>
 </body>
 </html>
 ...
@@ -31,16 +49,49 @@ my $index_tmpl = <<'...';
 <!doctype html>
 <html>
 <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title>tokuhirom's memo</title>
+    <meta http-equiv="Content-Style-Type" content="text/css" />  
+    <meta http-equiv="Content-Script-Type" content="text/javascript" />  
+    <link href="/static/css/reset.css" rel="stylesheet" type="text/css" />
+    <link href="/static/css/gpath.css" rel="stylesheet" type="text/css" />
+</head>
+<body class="Index">
+    <div id="container">
+        <div id="bodyContainer">
+        <h1>tokuhirom's memo</h1>
+        <ul>
+? for my $entry (@$entries) {
+            <li><a href="<?= $entry->{fname} ?>"><?= $entry->{title} ?>(<?= $entry->{mtime}->strftime('%Y-%m-%d(%a) %H:%M:%S') ?>)</a></li>
+? }
+        </ul>
+        <div class="updated">Last Modified: <?= $lastmodified ?></div>
+        </div>
+    </div>
+</body>
+</html>
+...
+my $menu_tmpl = <<'...';
+? my ($entries, $lastmodified) = @_;
+<!doctype html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>tokuhirom's memo</title>
+    <meta http-equiv="Content-Style-Type" content="text/css" />  
+    <meta http-equiv="Content-Script-Type" content="text/javascript" />  
 </head>
 <body>
-    <h1>tokuhirom's memo</h1>
+    <h3>entry list</h3>
     <ul>
 ? for my $entry (@$entries) {
-        <li><a href="<?= $entry->{fname} ?>"><?= $entry->{title} ?>(<?= $entry->{mtime}->strftime('%Y-%m-%d(%a) %H:%M:%S') ?>)</a></li>
+        <li><a href="<?= $entry->{fname} ?>"><?= $entry->{title} ?>(<?= $entry->{mtime}->strftime('%Y-%m-%d') ?>)</a></li>
 ? }
     </ul>
-    <div class="lastmodified">Last Modified: <?= $lastmodified ?></div>
+    <h3>link</h3>
+    <ul>
+        <li><a href="http://gp.ath.cx/">gpath</a></li>
+    </ul>
 </body>
 </html>
 ...
@@ -61,6 +112,7 @@ sub main {
                 next if $path =~ /\.sw[pon]$/;
                 render_entry($path);
                 render_index();
+                render_menu();
             }
         });
     }
@@ -71,6 +123,7 @@ sub first {
         render_entry($fname);
     }
     render_index();
+    render_menu();
 }
 
 sub render_entry {
@@ -101,10 +154,20 @@ sub render_index {
     while (my ($file, $attr) = each %$map) {
         push @files, {fname => $file, %$attr};
     }
-    @files = sort { $a->{mtime} <=> $b->{mtime} } @files;
+    @files = reverse sort { $a->{mtime} <=> $b->{mtime} } @files;
 
     my $html = render_mt($index_tmpl, \@files, DateTime->now->strftime('%Y-%m-%d(%a) %H:%M:%S'));
     write_file($html => "$outputdir/index.html");
+}
+sub render_menu {
+    my @files;
+    while (my ($file, $attr) = each %$map) {
+        push @files, {fname => $file, %$attr};
+    }
+    @files = reverse sort { $a->{mtime} <=> $b->{mtime} } @files;
+
+    my $html = render_mt($menu_tmpl, \@files, DateTime->now->strftime('%Y-%m-%d(%a) %H:%M:%S'));
+    write_file($html => "$outputdir/menu.html");
 }
 
 sub write_file {
